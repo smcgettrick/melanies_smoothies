6c4980cf2a04e7b8653b603ffaa8eb207129445c
Sean McGettrick <smcgettrick@gmail.com>
2025-08-19 13:02:57 -0400

    use search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,7 @@ session = cnx.session()
 fruits_df = session.table("smoothies.public.fruit_options").select(
     col("FRUIT_NAME"), col("SEARCH_ON")
 )
-
 pd_df = fruits_df.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", fruits_df, max_selections=5
@@ -29,9 +26,13 @@ if ingredients_list and name_on_order:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+
+        search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"].iloc[0]
+        st.write(f"The serach value for {fruit_chosen} is {search_on}.")
+
         st.subheader(fruit_chosen + " Nutrition Information")
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+            "https://my.smoothiefroot.com/api/fruit/" + search_on
         )
         sf_df = st.dataframe(
             data=smoothiefroot_response.json(), use_container_width=True
